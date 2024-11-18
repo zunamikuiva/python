73c5029a5e9b1c0f1f889e17a46c4a568a454332
zunamikuiva <nina.hinkkanen@hotmail.com>
2024-11-18 15:49:16 +0200

    tiedostonKasittelya
    Kokeilu LUT:in kurssilta

--- L06T3.py
+++ L06T3.py
@@ -1,29 +1,40 @@
+import re  # Tuodaan regular expression -kirjasto (re), jolla voi etsiä tekstiä tietyn kaavan mukaan
 
-import re
-nimi1=input("Anna luettavan tiedoston nimi: ")
-file = open(nimi1,'r')
-L1=len(re.findall(r"[\n']+", open(nimi1).read()))
-nimi2=input("Anna kirjoitettavan tiedoston nimi: ")
-open(nimi2,'w').write('')
+# Käyttäjä antaa tiedoston nimen, josta luetaan dataa
+nimi1 = input("Anna luettavan tiedoston nimi: ")
+file = open(nimi1, 'r')  # Avataan tiedosto lukutilassa ('r')
+
+# Lasketaan alkuperäisen tiedoston rivien määrä regexillä (kaikki '\n' -merkit)
+L1 = len(re.findall(r"[\n']+", open(nimi1).read()))
+
+# Käyttäjä antaa tiedoston nimen, johon kirjoitetaan suodatettu data
+nimi2 = input("Anna kirjoitettavan tiedoston nimi: ")
+open(nimi2, 'w').write('')  # Tyhjennetään kirjoitustiedosto ennen kirjoittamista
+
+# Määritellään pääohjelma-funktio
 def paaohjelma():
-    while (True):
-        f=file.readline().replace('\n', '')
+    while True:  # Silmukka, joka jatkaa kunnes tiedosto on luettu loppuun
+        # Luetaan rivi kerrallaan, ja poistetaan '\n' -merkit rivin lopusta
+        f = file.readline().replace('\n', '')
+        
+        # Jos rivi on tyhjä, on saavutettu tiedoston loppu
         if f == "":
-            L2=len(re.findall(r"[\n']+", open(nimi2).read()))
-            print("Luettiin " + str(L1) + " riviä tiedostosta '"+nimi1+"'.")
+            # Lasketaan kirjoitustiedoston (nimi2) rivien määrä lopuksi
+            L2 = len(re.findall(r"[\n']+", open(nimi2).read()))
+            print("Luettiin " + str(L1) + " riviä tiedostosta '" + nimi1 + "'.")
             print("Kirjoitettiin " + str(L2) + " riviä tiedostoon '" + nimi2 + "'.")
             print("Kiitos ohjelman käytöstä.")
-            break
+            break  # Poistutaan silmukasta ja lopetetaan ohjelma
         
+        # Tarkistetaan, sisältääkö rivi numeroita
         if any(ch.isdigit() for ch in f):
-            pass
+            pass  # Jos sisältää, hypätään tämän rivin yli
             
-        
         else:
-            open(nimi2,'a').write(f.lower()+'\n')
-        
+            # Jos rivi ei sisällä numeroita, se kirjoitetaan kirjoitustiedostoon pienin kirjaimin
+            open(nimi2, 'a').write(f.lower() + '\n')
     
-    
-   
-    file.close()
+    file.close()  # Suljetaan luettava tiedosto lopuksi
+
+# Suoritetaan pääohjelma-funktio
 paaohjelma()
